3b56fee814e6f67ff09d7c2597bd33f35042e496
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-03-23 18:00:21 +0100

    milestone 2 done

--- script.js
+++ script.js
@@ -4,14 +4,30 @@ Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni ic
 Milestone 2
 Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
 Milestone 3
-Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
-BONUS
-1- modificare la struttura dati fornita e valorizzare la proprietà "color" in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo "#" seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
-2- popolare le options della select della milestone 3 dinamicamente.
-Consigli del giorno
-Come sempre, iniziamo prima di tutto dall'analisi e comprensione della consegna. Scomponiamo il problema in micro-passaggi logici che solamente in un secondo momento trasformeremo in codice.
-Le icone presenti nella struttura dati fanno riferimento alla nota libreria Font Awesome, perciò come prima cosa assicuriamoci di aver inserito il link alla cdn nell'head della pagina.
-Dopodiché, basandoci sul codice di riferimento nel sito di Font Awesome, analizziamo come è formato il tag <i> di un'icona qualsiasi, in particolare focalizziamoci sulle classi.
-Come possiamo usare i dati presenti nella nostra struttura dati per creare l'elemento html nel modo corretto e visualizzare l'icona in pagina?
-Inizialmente può essere sufficiente stampare dei semplici div, senza alcuno stile, con all'interno l'icona e uno span con il nome. Solamente quando la parte logica è completa, ci dedichiamo al css.
-*/
+Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select,
+ visualizzare solamente le icone corrispondenti.*/
+
+
+
+const containerCard=document.querySelector('.row');
+
+function creoBox(icona){
+
+    let box = document.createElement('div');
+    box.className ="col-2 text-center m-4 bg-white p-3 rounded-2"
+    box.innerHTML =`
+    <div><i class="${icona.family} ${icona.prefix}${icona.name} ${icona.color} "></i>
+        <h2>${icona.name} </h2> 
+    </div>
+    `
+    containerCard.appendChild(box);
+}
+
+function start(){
+    for(let i=0; i <icons.length; i++ ){
+        const element = icons[i];
+        creoBox(element);
+    }
+}
+
+start();
